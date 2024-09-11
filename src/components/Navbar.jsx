3eec6041cbef71f3c5616f12cc5f0df92aba49ff
Dilshad Raja <dilshadraja236@gmail.com>
2024-09-11 12:48:27 +0530

    navbar some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,8 +31,8 @@ function Navbar() {
     return (
     <>
             <div className='bg-transparent space-y-3 flex justify-between  px-3 py-5 flex-wrap'>
-                <div className="nav1 flex flex-wrap gap-x-12 justify-center md:justify-between text-lg items-center ">
-                    <div className="logo text-white text-3xl font-bold leading-10 cursor-pointer">Dil<span className='text-red-600 mx-1.5 '>MOVIES </span></div>
+                <div className="nav1 flex flex-wrap gap-x-12 justify-center md:justify-between text-3xl lg:text-xl items-center ">
+                    <div className="logo text-white text-4xl  lg:text-3xl font-bold leading-10 cursor-pointer">Dil<span className='text-red-600 mx-1.5 '>MOVIES </span></div>
 
                     <div className="category text-white mx-3 md:mx-0">
                         <ul className="flex gap-3">
@@ -45,17 +45,17 @@ function Navbar() {
                     </div>
                 </div>
 
-               <div className="flex flex-wrap justify-center  md:justify-between  md:items-center md:space-x-7">
-                   <div className="nav2 w-[95vw] md:w-[28vw]  text-white border border-white text-lg rounded-2xl flex gap-2 items-center justify-between">
+               <div className="flex flex-wrap lg:flex-nowrap justify-center  md:w-fit md:justify-between  md:items-center md:space-x-7">
+                   <div className="nav2 w-[95vw] lg:w-[28vw]  text-white border border-white text-lg rounded-2xl flex gap-2 items-center justify-between">
                        <div className="flex items-center">
                         <FaMagnifyingGlass className='text-3xl  ml-2' />
-                        <input onChange = {inputHandler} className='px-1 py-2  w-[19vw] bg-transparent outline-none placeholder:text-white' type="text" placeholder='Search Movies Series And Others'  />
+                        <input onChange = {inputHandler} className='px-1 py-2  w-[55vw] lg:w-[19vw] bg-transparent outline-none placeholder:text-white' type="text" placeholder='Search Movies Series And Others'  />
                         </div>
                        <Link to={value.length != 0 ?'/searching-data' : ""}><button onClick={searchHandler} className="bg-white text-black py-2 px-4
                          rounded-e-xl">Search</button></Link>
                     </div>
 
-                    <div className="btn mt-4 md:mt-0 space-x-28 md:space-x-3 text-white text-lg md:flex justify-between items-center">
+                    <div className="btn mt-4 lg:mt-0 md:w-full lg:w-[39%] space-x-28 md:space-x-3 text-white text-lg flex justify-between items-center ">
                         <button className='px-5 py-2 rounded-lg border-2 border-white hover:bg-white hover:text-black mx-2' >Login</button>
                         <button className='px-5 py-2 rounded-lg border-2 border-white hover:bg-white hover:text-black mx-2' >Register</button>
                     </div>
